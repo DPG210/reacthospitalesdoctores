6be4f590d7fdfc5e5cfd49fadcec3cff8178a27c
DPG210 <diegogrego210@gmail.com>
2025-10-21 13:55:22 +0200

    cambio home

--- src/components/CreateHospital.js
+++ src/components/CreateHospital.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Global from '../Global'
-
 export default class CreateHospital extends Component {
     url=Global.apiHospital;
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1 style={{color:"green"}}>Home</h1>
+        <h1 style={{color:"blue"}}>Home</h1>
       </div>
     )
   }
